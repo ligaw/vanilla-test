7da3f206467017073aa0f92a50ceee5f9696744d
Pablo Puyat <pablo@cbrz.com>
2021-03-04 00:20:24 -0500

    Updated description column to be max 100 characters

--- tests/Unit/WidgetHasOptionalDescriptionTest.php
+++ tests/Unit/WidgetHasOptionalDescriptionTest.php
@@ -27,4 +27,26 @@ class WidgetHasOptionalDescriptionTest extends TestCase
         $this->assertEquals(null, $widget->description);
     }
 
+    public function test_widget_description_max_one_hundred_characters()
+    {
+        $this->expectException(\Illuminate\Database\QueryException::class);
+        $this->expectExceptionMessageMatches('/data too long/i');
+
+        $description = '1234567890'    // 10 chars
+            . '1234567890'      // 20 chars
+            . '1234567890'      // 30 chars
+            . '1234567890'      // 40 chars
+            . '1234567890'      // 50 chars
+            . '1234567890'      // 60 chars
+            . '1234567890'      // 70 chars
+            . '1234567890'      // 80 chars
+            . '1234567890'      // 90 chars
+            . '1234567890'      // 100 chars
+            . '1';              // 101 chars
+
+        $widget = Widget::factory()->create([
+            'name' => 'Test Widget',
+            'description' => $description
+        ]);
+    }
 }
